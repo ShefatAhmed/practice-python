0e3e2ba226b96a7573d41ec0fa6c1eb282a8cf98
Shefat Ahmed <shefata69@gmail.com>
2025-01-01 16:46:04 +0600

    Variables & Data Types practice

---
+++ Variables & Data Types/FirstProgram.py
@@ -0,0 +1,42 @@
+# print("Hello world!"); # print in python
+
+# Variable
+# name= "shefat"
+# age = 24
+# price=25.45
+# print(age) # print variable
+# print(type(name), type(age), type(price)) # print of variable type
+
+# arithemtic operator
+# a = 1000
+# b= 500
+# print(a+b) # print sum
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) # reminder
+# print(a**b) # a^b
+
+# relational operators
+# == != >= > <= <
+
+# assignment operators
+# =, +=, -=, *=, /=, %=, **=
+
+# logical operators not, and, or
+# print(not False)
+# print(not(a>b))
+# val1= True
+# val2= True
+# print(val1 and val2)
+# print(a==b or a>b)
+
+# type conversion
+# x= "5"
+# y= 10
+# print(float(x)+y) 
+# print(int(x)+y) 
+
+# Inputs in Python
+# name = input("Enter your name:")
+# print(name)
