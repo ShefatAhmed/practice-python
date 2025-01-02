dd617ea69d344113ba9869f837901483d761688a
Shefat Ahmed <shefata69@gmail.com>
2025-01-02 22:00:15 +0600

    practice string and condition

---
+++ Strings & Conditional Statements/ScondProgram.py
@@ -0,0 +1,53 @@
+# strings 
+# str1 = "this is string in python"
+# str2 = "this is line break \n string"
+# two_str= str1 +str2
+# # print(two_str)
+# print(len(two_str)) # calculate length
+
+# indexing 
+# str = "shefat"
+# print(str[2]) # indexing character 
+# print(str[:2]) # get 0 to 2 number index value 
+# print(str[1:4]) # get 1 to 3 number index value 
+
+# negative indexing 
+# str = "shefat"
+# print(str[-4: -1]) # this feature work only python language
+# print(str.count("e")) # how many e here in this str
+
+
+# Conditional Statements
+# light = "green";
+# if(light == "red"):
+#         print("stop")
+# elif(light == "blue"):
+#         print("go")
+# elif(light == "green"):
+#         print("look")
+# else: 
+#         print("light is broken")
+# print("end of code")
+
+# marks = int(input("Enter you marks:"))
+# if(marks >= 80):
+#         grade = "A+"
+# elif(marks >= 70):
+#         grade = "B"
+# elif(marks >= 60):
+#         grade = "C"
+# elif(marks >= 50):
+#         grade = "D"
+# else: 
+#         grade = "F"
+# print("Your result is: ", grade)
+
+
+# Problem
+# 1. Check if a number entered by the user is odd or even.
+number = int(input("Enter a number:"))
+reminder = number % 2
+if(reminder == 0):
+        print("number is even")
+else: 
+        print("number is odd")
