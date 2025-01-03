f6d13035c7ea9e0d59ec47e8719d00379ba50b5d
Shefat Ahmed <shefata69@gmail.com>
2025-01-03 21:38:06 +0600

    practice list and tuples

---
+++ List & Tuple in Python/ThirdProgram.py
@@ -0,0 +1,14 @@
+# list 
+# marks = [94.5, 87.5, 95.2, 66.4, 45.1]
+# print(type(marks))
+
+# list methods
+# list = [2,1,3]
+# print(list.append(4))
+# print(list.sort(reverse=True))
+# print(list.reverse)
+# print(list)
+
+# tuples in Python
+# tup = (2,1,3,4)
+# print(tup[0])
