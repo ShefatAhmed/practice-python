b8fc7f3b6306ecadcfbce5e534e60cb004a5269b
Shefat Ahmed <shefata69@gmail.com>
2025-01-04 19:49:33 +0600

    practice dictionary and set

---
+++ Dictionary & Set/FourthProgram.py
@@ -0,0 +1,33 @@
+# info = {
+#         "fullName": {
+#                 "firstName":"shefat",
+#                 "lastName": "ahmed"
+#         },
+#         "subjects": ["javascript", "python", "typescript"],
+#         "topics": ("dict", "set"),
+#         "age": 24,
+#         "is_adult": True,
+#         12.99: 94.4
+# }
+# print(type(info))
+
+# collection = set()
+# collection.add(1)
+# print(collection)
+
+# pb1
+# marks = {}
+# x = int(input("enter phy: "))
+# marks.update({"phy": x})
+# x = int(input("enter math: "))
+# marks.update({"math": x})
+# x = int(input("enter chem: "))
+# marks.update({"chem": x})
+# print(marks)
+
+# pb2
+# values ={
+#         ("float", 9.0),
+#         ("int", 9)
+# }
+# print(values)
