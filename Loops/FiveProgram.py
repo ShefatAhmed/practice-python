ba12376cbfd2a7a28167dfdc01085e61c80b1dfe
Shefat Ahmed <shefata69@gmail.com>
2025-01-05 21:13:16 +0600

    practice loops

---
+++ Loops/FiveProgram.py
@@ -0,0 +1,30 @@
+# loops
+# count = 1;
+# while count <=5:
+#         print("hello")
+#         count +=1
+# i = 1;
+# while i <= 10:
+#         print(i)
+#         i+=1
+
+nums = [1,4,9,16,25,36,49,64,81,100]
+heros = ["ironman", "thor", "superman", "batman"]
+
+# idx = 0
+# while idx < len(heros):
+#         print(heros[idx])
+#         idx +=1
+
+# idx = 0
+# while idx <len(nums):
+#         print(nums[idx]) 
+#         idx +=1
+
+# str = "shefatahmed"
+# for char in str:
+#         if(char == "a"):
+#                 print("a found"
+#                       break
+#         print(char)
+# print("END"))
