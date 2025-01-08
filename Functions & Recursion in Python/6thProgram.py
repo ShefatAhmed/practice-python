4ae8f2f63aef5c8aa94011842825ee029f389cc5
Shefat Ahmed <shefata69@gmail.com>
2025-01-08 22:07:14 +0600

    practice recursion

--- Functions & Recursion in Python/6thProgram.py
+++ Functions & Recursion in Python/6thProgram.py
@@ -9,3 +9,8 @@
 # for i in range(1,2+1):
 #         fact *=i
 #         print(fact)
+
+# def converter(usd_val):
+#         inr_val = usd_val * 83
+#         print(usd_val, inr_val)
+# converter(80)
