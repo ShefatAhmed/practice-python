618f7247653b71d1385861f429cf8873988a3599
Shefat Ahmed <shefata69@gmail.com>
2025-01-11 14:05:11 +0600

    practice recursion

--- Functions & Recursion in Python/6thProgram.py
+++ Functions & Recursion in Python/6thProgram.py
@@ -28,3 +28,11 @@
 #         calc_sum(n-1) + n
 
 # calc_sum(5)
+
+# def print_list(list, idx=0):
+#         if(idx == len(list)):
+#                 return 
+#         print(list[idx])
+#         print_list(list, idx+1)
+# fruits = ["mango", "litchi", "apple", "banana"]
+# print_list(fruits)
