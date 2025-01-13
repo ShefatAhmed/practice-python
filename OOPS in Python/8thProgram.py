adb831efc52c243bd5388dda7291de1f1d19280d
Shefat Ahmed <shefata69@gmail.com>
2025-01-13 21:12:57 +0600

    practice oops

--- OOPS in Python/8thProgram.py
+++ OOPS in Python/8thProgram.py
@@ -2,3 +2,15 @@
 #         name= "shefat"
 # s1 = Student()
 # print(s1)
+
+# class Student:
+#         def __init__(self, name, marks):
+#            self.name =  name
+#            self.marks = marks
+#         def get_avg(self):
+#               sum = 0
+#               for val in marks:
+#                     sum +=val
+#                     print("h1", self.name, "your avg score is:", sum/3)
+# s1 = Student("tony stark", [99, 98, 97])
+# s1.get_avg()
