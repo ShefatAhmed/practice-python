e521e90d033947aecfe8d27b5785a162603f8e13
Shefat Ahmed <shefata69@gmail.com>
2025-01-14 20:52:32 +0600

    practice oops

--- OOPS in Python/8thProgram.py
+++ OOPS in Python/8thProgram.py
@@ -14,3 +14,16 @@
 #                     print("h1", self.name, "your avg score is:", sum/3)
 # s1 = Student("tony stark", [99, 98, 97])
 # s1.get_avg()
+
+# def __init__(self):
+#         self.acc =  False
+#         self.brk = False
+#         self.clutch = False
+
+# def start(self):
+#         self.clutch = True
+#         self.acc = True
+#         print("car started...")
+
+# car1 = Car()
+# car1.start())
