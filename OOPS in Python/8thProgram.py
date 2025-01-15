43351a1275f9ccf9576535c85e80daec005a585f
Shefat Ahmed <shefata69@gmail.com>
2025-01-15 20:54:52 +0600

    practice oops

--- OOPS in Python/8thProgram.py
+++ OOPS in Python/8thProgram.py
@@ -27,3 +27,11 @@
 
 # car1 = Car()
 # car1.start())
+
+# class Account:
+#         def __init__(self, bal, acc):
+#                 self.balance = bal
+#                 self.account_no = acc
+# acc1 = Account(10000, 12345)
+# print(acc1.balance)
+# print(acc1.account_no)
